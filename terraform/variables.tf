3eaa4d77e2ca813c293e5c8d84baf587f61a97e9
Ben Kooijman <ben@kooijman.se>
2022-03-11 10:07:50 +0100

    update tf files

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable primary_azure_subscription_id {
+  type        = string
+  description = "description"
+}
+
+variable primary_azure_client_id {
+  type        = string
+  description = "description"
+}
+
+variable primary_azure_client_secret {
+  type        = string
+  description = "description"
+}
+
+variable primary_azure_tenant_id {
+  type        = string
+  description = "description"
+}
