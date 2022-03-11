175580df52f85756642f49cbb489c7d5b7e8a2c0
Ben Kooijman <100195282+benkoben@users.noreply.github.com>
2022-03-11 15:46:19 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "connectivity" {
 }
 
 resource "azurerm_virtual_network" "connectivity" {
-  name                = ""
+  name                = "vnet-connectivityhub-weeu-${var.env}-001"
   location            = azurerm_resource_group.connectivity.location
   resource_group_name = azurerm_resource_group.connectivity.name
   address_space       = ["10.0.0.0/16"]
